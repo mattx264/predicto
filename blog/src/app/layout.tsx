70d84e048dbe17e359df6977d18f48aebf1dd78d
mattx mattx <mattx264@yahoo.com>
2025-10-17 05:49:37 -0500

    add google analytics and remove express js from package.json

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/nav/Navbar";
 import Footer from "./components/footer/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
 
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-WY4RP7HZDG" />
     </html>
   );
 }
