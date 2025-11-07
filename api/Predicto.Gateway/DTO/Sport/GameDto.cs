f444be48ceeb67c2e4cd025676b5d04473b19bdd
mattx mattx <mattx264@yahoo.com>
2025-11-07 11:17:24 -0600

    Cleanup models

--- api/Predicto.Gateway/DTO/Sport/GameDto.cs
+++ api/Predicto.Gateway/DTO/Sport/GameDto.cs
@@ -1,11 +1,49 @@
 ﻿namespace Predicto.Gateway.DTO.Sport
 {
-    public class GameDto
+    public class GameDetailsDto
     {
         public int Id { get; set; }
-        public int TournamentId { get; set; }
-        public required string FinalScoore { get; set; }// 1:1 (4:5) //
-        public DateTime StartGame { get; set; }
-        public DateTime? EndGame { get; set; }
+        public DateTime StartTime { get; set; }
+        public IList<GameDetailsTeamDto> Teams { get; set; }
+        public string? FinalScore { get; set; }
+        public int TournamentId { get; internal set; }
+        public string? Referee { get; internal set; }
+        public IList<HeadToHeadDto> HeadToHead { get; set; }
     }
+    public class GameDetailsTeamDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LogoUrl { get; set; }
+        public string? Tactic { get; set; }
+        public IList<PlayerBasicInfoDto> Players { get; set; }
+        public string Coach { get; internal set; }
+        public string FormLastGames { get; internal set; }
+    }
+    public class HeadToHeadDto
+    {
+        public int GameId { get; set; }
+        public DateTime GameDate { get; set; }
+        public string FinalScore { get; set; }
+        public int TeamId1 { get; set; }
+        public int TeamId2 { get; set; }
+        public string TeamName1 { get; set; }
+        public string TeamName2 { get; set; }
+    }
+    public class GameListDto
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public IList<GameListTeamDto> Teams { get; set; }
+        public string? FinalScore { get; set; }
+
+    }
+    public class GameListTeamDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LogoUrl { get; set; }
+    }
+  
 }
--- api/Predicto.Gateway/Services/GameService.cs
+++ api/Predicto.Gateway/Services/GameService.cs
@@ -79,53 +79,6 @@ namespace Predicto.Gateway.Services
         }
 
     }
-
-
-    public class GameDetailsDto
-    {
-        public int Id { get; set; }
-        public DateTime StartTime { get; set; }
-        public IList<GameDetailsTeamDto> Teams { get; set; }
-        public string? FinalScore { get; set; }
-        public int TournamentId { get; internal set; }
-        public string? Referee { get; internal set; }
-        public IList<HeadToHeadDto> HeadToHead { get; set; }
-    }
-    public class GameDetailsTeamDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string LogoUrl { get; set; }
-        public string? Tactic { get; set; }
-        public IList<PlayerBasicInfoDto> Players { get; set; }
-        public string Coach { get; internal set; }
-        public string FormLastGames { get; internal set; }
-    }
-    public class HeadToHeadDto
-    {
-        public int GameId { get; set; }
-        public DateTime GameDate { get; set; }
-        public string FinalScore { get; set; }
-        public int TeamId1 { get; set; }
-        public int TeamId2 { get; set; }
-        public string TeamName1 { get; set; }
-        public string TeamName2 { get; set; }
-    }
-    public class GameListDto
-    {
-        public int Id { get; set; }
-        public DateTime StartTime { get; set; }
-
-        public IList<GameListTeamDto> Teams { get; set; }
-        public string? FinalScore { get; set; }
-
-    }
-    public class GameListTeamDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string LogoUrl { get; set; }
-    }
     public interface IGameService
     {
         Task<List<GameListDto>> GetAll();
